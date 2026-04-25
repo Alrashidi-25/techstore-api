968cd5142e5adc99419793f157f4b5ceb72b0a0a
ALrshidi <aabbdd20192019@gmail.com>
2026-04-25 21:28:50 +0300

    Implement CORS configuration and add web.config for Laravel routing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,36 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->configureCors();
+        $this->configureSanctumStatefulDomains();
+    }
+
+    protected function configureCors(): void
+    {
+        $allowedOrigins = env('CORS_ALLOWED_ORIGINS', '*');
+        
+        if ($allowedOrigins === '*') {
+            config(['cors.supports_credentials' => false]);
+        } else {
+            config([
+                'cors.supports_credentials' => true,
+                'cors.allowed_origins' => array_map('trim', explode(',', $allowedOrigins)),
+                'cors.allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+                'cors.allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+                'cors.exposed_headers' => [],
+                'cors.max_age' => 86400,
+            ]);
+        }
+    }
+
+    protected function configureSanctumStatefulDomains(): void
+    {
+        $statefulDomains = config('sanctum.stateful', []);
+        $additionalDomains = env('SANCTUM_STATEFUL_DOMAINS', '');
+        
+        if (!empty($additionalDomains)) {
+            $domains = array_merge($statefulDomains, array_map('trim', explode(',', $additionalDomains)));
+            config(['sanctum.stateful' => array_unique($domains)]);
+        }
     }
 }
